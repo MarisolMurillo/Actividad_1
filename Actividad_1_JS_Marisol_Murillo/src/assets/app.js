29d5e022a90d27a01ab47bb4631dbfdf4283fddf
MarisolMurillo <105325934+MarisolMurillo@users.noreply.github.com>
2022-07-26 17:56:27 -0500

    Add files via upload

---
+++ Actividad_1_JS_Marisol_Murillo/src/assets/app.js
@@ -0,0 +1,23 @@
+let name = 'Marisol',
+  lastName = 'Murillo',
+  YearOfBirth = 2000,
+  cityOfBirth = 'Medellín'
+
+console.log(
+  `Hola, mi nombre es ${name}
+${lastName}, vivo en la ciudad de ${cityOfBirth}, y tengo ${
+    2022 - YearOfBirth
+  } años.`
+)
+
+/* let a = 10,
+  e = false,
+  i = 'string',
+  u = null
+
+console.log(typeof a)
+console.log(typeof e)
+console.log(typeof i)
+console.log(typeof o)
+console.log(typeof u)
+ */
